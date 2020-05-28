5ca0c6cd613002358a5ef239b1d62a14d6994cf3
Caleb Bridges <calebbridges97@gmail.com>
2020-05-28 07:47:44 -0400

    second angular lab

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'ShadowMan' },
+  { id: 12, name: 'Apex' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Onyx' },
+  { id: 15, name: 'Captain Bear' },
+  { id: 16, name: 'PlastiGirl' },
+  { id: 17, name: 'Dynamite' },
+  { id: 18, name: 'Dr Eclipse' },
+  { id: 19, name: 'Mistique' },
+  { id: 20, name: 'The Bulk' }
+];
